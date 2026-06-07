dc35cd170bd14806a5c08b0af31c94867ffd8342
Madhu Priya K B <madhupriyakb2006@gmail.com>
2026-06-07 16:03:48 +0530

    update api url for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ecommerce-fullstack-hrqy.onrender.com/api',
   withCredentials: true
 });
 
-// Automatically attach token to every request
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user'));
   if (user && user.token) {
